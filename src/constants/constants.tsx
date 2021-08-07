852223c1b41825dc7f93a6306a6032dfc9bf3e93
Faris Ashai <farisashai@gmail.com>
2021-08-07 13:27:23 -0700

    country icons

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,3 +1,19 @@
+import { ReactComponent as Spain } from 'assets/spain.svg';
+import { ReactComponent as China } from 'assets/china.svg';
+import { ReactComponent as Japan } from 'assets/japan.svg';
+import { ReactComponent as France } from 'assets/france.svg';
+import { ReactComponent as Korea } from 'assets/korea.svg';
+import { ReactComponent as India } from 'assets/india.svg';
+
 export const one = 'one';
 
 export const two = 'two';
+
+export const icons = {
+  SPAIN: Spain,
+  CHINA: China,
+  FRANCE: France,
+  JAPAN: Japan,
+  KOREA: Korea,
+  INDIA: India,
+};
